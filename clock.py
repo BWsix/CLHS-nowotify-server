88efba52e2ca7ad259a4bae6cd1e0dd4f5a19402
BWsix <itisbw62@gmail.com>
2021-08-09 19:58:53 +0800

    update: change the server running duration

--- clock.py
+++ clock.py
@@ -7,8 +7,8 @@ from functions import checker
 
 sched = BlockingScheduler()
 
-# 因為經費有限，只能在每天的上午7點到晚上7點運作
-@sched.scheduled_job('cron', hour='7-19', minute='*/5')
+# 因為經費有限，只能在每天的上午7點到晚上6點半運作
+@sched.scheduled_job('cron', hour='7-17', minute='*/5')
 def scheduled_job():
   get(os.environ["url"])
 
