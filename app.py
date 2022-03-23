99a987e82411b51ff8fe96ba33b5b169a17e0b40
VFLC <itisbw62@gmail.com>
2022-03-24 01:00:11 +0800

    running out of heroku free dyno hours :0

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-
+from functions import checker
 
 app = Flask(__name__)
 
 @app.route('/')
 def dummy():
+  checker()
+  
   return "a dummy site, nothing here."
 
 if __name__ == "__main__":
